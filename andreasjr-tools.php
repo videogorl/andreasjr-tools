f2842c2f6e08475991ecfc8efbac00e1371ccd58
Andreas Reif <andreas@sitecare.com>
2023-12-14 23:25:33 -0700

    Add CPTs for projects and portfolio Add taxonomy for projects Register blocks Register admin styles Register custom meta fields for projects

--- andreasjr-tools.php
+++ andreasjr-tools.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name:       Trading Card
+ * Plugin Name:       andreasjr-tools
  * Plugin URI:        https://andreasjr.com
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.1
@@ -25,7 +25,196 @@ if ( ! defined( 'ABSPATH' ) ) {
  *
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
-function andreasjr_tools_andreasjr_tools_block_init() {
-	register_block_type( __DIR__ . '/build' );
-}
-add_action( 'init', 'andreasjr_tools_andreasjr_tools_block_init' );
+add_action( 'init', function() {
+	foreach (glob( plugin_dir_path( __FILE__ ) . 'blocks/build/*', GLOB_ONLYDIR ) as $folder) {
+	
+		/**
+		 * Find out if block has a helper
+		 */
+		$helper_url = $folder . '/helper.php';
+		if (file_exists( $helper_url )) {
+			include $helper_url;
+		}
+	
+		register_block_type( $folder );
+	}
+} );
+
+
+// Enqueue Editor Assets
+add_action( 'enqueue_block_editor_assets', function() {
+
+	foreach (glob( plugin_dir_path( __FILE__ ) . 'editor/build/*', GLOB_ONLYDIR ) as $folder) {
+		$dep_name = explode('/', $folder);
+		$dep_name = $dep_name[ count($dep_name) - 1 ];
+
+		/**
+		 * Find out if plugin has a helper
+		 */
+		$helper_url = $folder . '/helper.php';
+		if (file_exists( $helper_url )) {
+			include $helper_url;
+		}
+		
+		/**
+		 * Register script
+		 */
+		$dependencies = require($folder . '/index.asset.php');
+		wp_enqueue_script(
+			'appalachia-plugins-' . $dep_name . '-script', // unique handle
+			plugin_dir_url( __FILE__ ) . 'editor/build/' . $dep_name . '/index.js',
+			$dependencies['dependencies'],
+			$dependencies['version']
+		);
+	}
+} );
+
+/**
+ * Add custom post types
+ */
+
+add_action( 'init', function() {
+	/**
+	 * Post Type: Portfolio.
+	 */
+	register_post_type( "portfolio-gallery", [
+		"label" => esc_html__( "Portfolio", "inkling" ),
+		"labels" => [
+			"name" => esc_html__( "Portfolio", "inkling" ),
+			"singular_name" => esc_html__( "Piece", "inkling" ),
+		],
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"can_export" => true,
+		"rewrite" => [
+			"slug" => "portfolio-beta",
+			"with_front" => false
+		],
+		"query_var" => true,
+		"menu_icon" => "dashicons-excerpt-view",
+		"supports" => [ "title", "editor", "thumbnail", "excerpt", "page-attributes" ],
+		"taxonomies" => [ "category", "post_tag" ],
+		"show_in_graphql" => true,
+		"has_archive" => "portfolio-beta"
+	] );
+
+	/**
+	 * Post Type: Projects.
+	 */
+	register_post_type( "project-gallery", [
+		"label" => esc_html__( "Projects", "inkling" ),
+		"labels" => [
+			"name" => esc_html__( "Projects", "inkling" ),
+			"singular_name" => esc_html__( "Project", "inkling" ),
+		],
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"can_export" => true,
+		"rewrite" => [ "slug" => "project", "with_front" => false ],
+		"query_var" => true,
+		"menu_icon" => "dashicons-excerpt-view",
+		"supports" => [ "title", "editor", "thumbnail", "excerpt", "custom-fields" ],
+		"taxonomies" => [ "project-gallery-type" ],
+		"show_in_graphql" => false,
+		"has_archive" => "projects-beta"
+	] );
+
+	register_taxonomy( "project-gallery-type", [ "project-gallery" ], [
+		"label" 				=> esc_html__( "Project Type", "andreasjr-filament" ),
+		"labels" 				=> [
+			"name" 					=> esc_html__( "Project Types", "andreasjr-filament" ),
+			"singular_name" 		=> esc_html__( "Project Type", "andreasjr-filament" ),
+			"add_new_item"			=> esc_html__( "Add New Type", "andreasjr-filament "),
+			"parent_item"			=> esc_html__( "Parent Type", "andreasjr-filament "),
+		],
+		"public" 				=> true,
+		"publicly_queryable" 	=> true,
+		"hierarchical" 			=> true,
+		"show_ui" 				=> true,
+		"show_in_menu" 			=> true,
+		"show_in_nav_menus" 	=> true,
+		"query_var" 			=> true,
+		"rewrite" 				=> [ 'slug' => 'type', 'with_front' => false, ],
+		"show_admin_column" 	=> true,
+		"show_in_rest" 			=> true,
+		"show_tagcloud" 		=> false,
+		"show_in_quick_edit" 	=> true,
+		"sort" 					=> false,
+		"show_in_graphql" 		=> false,
+	] );
+
+
+	// Resource URL
+	register_meta('post', 'resource_url', [
+		'type' 		=> 'string',
+		'single' 	=> true,
+		'show_in_rest' => true,
+		'object_subtype' => 'project-gallery'
+	]);
+	// Resource Description
+	register_meta('post', 'resource_description', [
+		'type' 		=> 'string',
+		'single' 	=> true,
+		'show_in_rest' => true,
+		'object_subtype' => 'project-gallery'
+	]);
+	// Resource Image
+	register_meta('post', 'resource_image', [
+		'type' => 'object',
+		'single' => true,
+		'show_in_rest' => array(
+			'schema' => array(
+				'type'		=> 'object',
+				'properties' => array(
+					'id'  => array('type' => 'number'),
+					'url' => array('type' => 'string'),
+					'accent_color' => array('type' => 'string')
+				),
+				'additionalProperties' => true
+			),
+		),
+	]);
+
+	
+} );
+
+add_action( 'enqueue_block_editor_assets', function() {
+	/**
+	 * Find out if plugin has a helper
+	 */
+	$helper_url = plugin_dir_path( __FILE__ ) . 'editor/build/helper.php';
+	if (file_exists( $helper_url )) {
+		include $helper_url;
+	}
+	
+	/**
+	 * Register script
+	 */
+	$dependencies = require(plugin_dir_path( __FILE__ ) . 'editor/build/index.asset.php');
+	wp_enqueue_script(
+		'andreasjr-tools-editor-script', // unique handle
+		plugin_dir_url( __FILE__ ) . 'editor/build/index.js',
+		$dependencies['dependencies'],
+		$dependencies['version']
+	);
+} );
