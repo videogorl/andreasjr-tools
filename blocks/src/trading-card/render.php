0a703abebd4c0fb556c716c4d4b51c7a4aa382d2
Andreas Reif <andreas@sitecare.com>
2023-12-15 00:05:29 -0700

    Update button styles

--- blocks/src/trading-card/render.php
+++ blocks/src/trading-card/render.php
@@ -4,8 +4,8 @@
  */
 
 $resource_url = get_metadata('post', $block->context['postId'], 'resource_url', true) ?? null;
-$resource_image = get_metadata('post', $block->context['postId'], 'resource_image', true);
-$resource_description = get_metadata('post', $block->context['postId'], 'resource_description', true);
+$resource_image = get_metadata('post', $block->context['postId'], 'resource_image', true) ?? null;
+$resource_description = get_metadata('post', $block->context['postId'], 'resource_description', true) ?? null;
 
 $permalink = get_permalink($block->context['postId']);
 
@@ -15,17 +15,17 @@ $permalink = get_permalink($block->context['postId']);
 		<figure class="content-header--featured-image-background">
 			<?= get_the_post_thumbnail($block->context['postId'], 'full' ) ?>
 		</figure>
-		<figure class="content-header--resource-image"><a href="<?=$permalink?>">
+		<?php if ($resource_image) : ?><figure class="content-header--resource-image"><a href="<?=$permalink?>">
 			<?= wp_get_attachment_image( $resource_image['id'], 'full' ) ?>
-		</a></figure>
+		</a></figure><?php endif; ?>
 	</div>
 
 	<div class="content-title"><h2><?= get_the_title($block->context['postId']) ?></h2></div>
 	<?= $resource_description ? '<div class="content-description">' . $resource_description . '</div>' : '' ?>
 
-	<div class="content-actions">
-		<a href="<?=$permalink?>" class="wp-element-button">Learn More</a>
-		<a href="<?= $resource_url ?>" target="_blank" class="wp-element-button">Get Shortcut</a>
+	<div class="content-actions wp-block-buttons is-layout-flex wp-block-buttons-is-layout-flex">
+		<div class="wp-block-button is-style-outline"><a href="<?=$permalink?>" class="wp-block-button__link wp-element-button">Learn More</a></div>
+		<?php if ($resource_url): ?><div class="wp-block-button"><a href="<?= $resource_url ?>" target="_blank" class="wp-element-button">Get Shortcut</a></div><?php endif; ?>
 	</div>
 	
 </div>
