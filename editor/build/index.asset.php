5cc9ee769dff4a6cc70d6974bcf4e93f37186408
Andreas Reif <andreas@sitecare.com>
2023-12-14 23:26:30 -0700

    Latest build

---
+++ editor/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins'), 'version' => '5708d2501bda8f463a51');
